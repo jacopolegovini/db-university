e2532a3bc8c474732b8b2f510ab9691f74d15c04
jacopolegovini <legovini96@gmail.com>
2024-10-17 16:27:34 +0200

    group by request 3 completed

--- university2.sql
+++ university2.sql
@@ -61,6 +61,10 @@ FROM `teachers`
 GROUP BY `office_address`;
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
-
+SELECT AVG(`exam_student`.`vote`) as "Voto medio", `exams`.`date` as "Sessione"
+FROM `exams`
+JOIN `exam_student`
+ON `exams`.`id` = `exam_student`.`exam_id`
+GROUP BY `exams`.`date`;
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
