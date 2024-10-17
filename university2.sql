525a9d7e5217658103d2d15b0d7d6826812f7ed0
jacopolegovini <legovini96@gmail.com>
2024-10-17 16:19:01 +0200

    join request 6 completed

--- university2.sql
+++ university2.sql
@@ -36,6 +36,14 @@ JOIN `courses` ON `course_teacher`.`course_id` = `courses`.`id`
 JOIN `degrees` on `courses`.`degree_id` = `degrees`.`id`;
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+SELECT * 
+FROM `teachers`
+JOIN `course_teacher` ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses` ON `course_teacher`.`course_id` = `courses`.`id`
+JOIN `degrees` ON `courses`.`degree_id` = `degrees`.`id`
+JOIN `departments`ON `degrees`.`department_id` = `departments`.`id`
+WHERE `departments`.`name` = "Dipartimento di Matematica";
+
 -- 7. BONUS: Selezionare per ogni studente il numero di tentativi sostenuti per ogni esame, stampando anche il voto massimo. Successivamente, filtrare i tentativi con voto minimo 18.
 
 
