fbb6c4eac3c9cbdb64c1dbf210f0300efa9aa736
jacopolegovini <legovini96@gmail.com>
2024-10-17 16:04:44 +0200

    join request 4 completed

--- university2.sql
+++ university2.sql
@@ -22,9 +22,18 @@ ON `course_teacher`.`course_id` = `courses`.`id`
 WHERE `teachers`.`id` = 44;
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+SELECT * 
+FROM `students`
+JOIN `degrees` ON `students`.`degree_id` = `degrees`.`id`
+JOIN `departments` ON `degrees`.`department_id` = `departments`.`id`
+ORDER BY `students`.`surname`, `students`.`name`; 
+
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 -- 7. BONUS: Selezionare per ogni studente il numero di tentativi sostenuti per ogni esame, stampando anche il voto massimo. Successivamente, filtrare i tentativi con voto minimo 18.
+
+
+----------------------------------------------------------------------------------------------------
 
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
